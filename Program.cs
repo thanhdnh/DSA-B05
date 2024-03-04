83201e3842373d508bb08abc75e0d3a117fe76e7
thanhdnh <myhoangthanh@yahoo.com>
2024-03-04 14:47:37 +0700

    OK

--- Program.cs
+++ Program.cs
@@ -3,51 +3,97 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        int[]arr = new int[1000];
+        int[] arr = new int[1000];
         Randomize(arr);
         int time = 0;
+        int[] cpy = new int[1000];
+
         Timing timing = new Timing();
-        for(int i=0; i<100; i++){
+        arr.CopyTo(cpy, 0);
+        for (int i = 0; i < 100; i++)
+        {
             timing.startTime();
             SelectionSort(arr);
             timing.StopTime();
             time += timing.Result().Milliseconds;
         }
-        Console.WriteLine($"Selection Sort: {time/100f} ms");
+        Console.WriteLine($"Selection Sort: {time / 100f} ms");
         time = 0;
-        for(int i=0; i<100; i++){
+        arr.CopyTo(cpy, 0);
+        for (int i = 0; i < 100; i++)
+        {
             timing.startTime();
             BubbleSort(arr);
             timing.StopTime();
             time += timing.Result().Milliseconds;
         }
-        Console.WriteLine($"Bubble Sort: {time/100f} ms");
-    
-    }
-    static void BubbleSort(int[] arr){
-        for(int i=0; i<arr.Length-1; i++)
-            for(int j=0; j<arr.Length-i-1; j++)
-                if(arr[j] > arr[j+1])
-                    Swap(ref arr[j], ref arr[j+1]);
+        Console.WriteLine($"Bubble Sort: {time / 100f} ms");
+        time = 0;
+        arr.CopyTo(cpy, 0);
+        for (int i = 0; i < 100; i++)
+        {
+            timing.startTime();
+            QuickSort(arr, 0, arr.Length - 1);
+            timing.StopTime();
+            time += timing.Result().Milliseconds;
+        }
+        Console.WriteLine($"Quick Sort: {time / 100f} ms");
     }
 
-  static void Randomize(int[] arr){
+    static void QuickSort(int[] arr, int low, int high)
+    {
+        int p = 0;
+        if (low < high)
+            p = Partition(arr, low, high);
+        if (p > 1)
+            QuickSort(arr, low, p - 1);
+        if (p + 1 < high)
+            QuickSort(arr, p + 1, high);
+    }
+    static int Partition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high];
+        int j = low;
+        for (int i = low; i < high; i++)
+            if (arr[i] < pivot)
+            {
+                Swap(ref arr[i], ref arr[j]);
+                i++;
+            }
+        Swap(ref arr[j], ref arr[high]);
+        return j;
+    }
+
+    static void BubbleSort(int[] arr)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+            for (int j = 0; j < arr.Length - i - 1; j++)
+                if (arr[j] > arr[j + 1])
+                    Swap(ref arr[j], ref arr[j + 1]);
+    }
+
+    static void Randomize(int[] arr)
+    {
         Random r = new Random();
-        for(int i=0; i<arr.Length; i++)
+        for (int i = 0; i < arr.Length; i++)
             arr[i] = r.Next(99);
     }
 
-    static void SelectionSort(int[] array){
-        for (int i = 0; i < array.Length - 1; i++){
+    static void SelectionSort(int[] array)
+    {
+        for (int i = 0; i < array.Length - 1; i++)
+        {
             int smallest = i;
-            for (int j = i + 1; j < array.Length; j++){
+            for (int j = i + 1; j < array.Length; j++)
+            {
                 if (array[j] < array[smallest])
                     smallest = j;
             }
             Swap(ref array[i], ref array[smallest]);
         }
     }
-    static void Swap(ref int v1, ref int v2){
+    static void Swap(ref int v1, ref int v2)
+    {
         int c = v1;
         v1 = v2;
         v2 = c;
